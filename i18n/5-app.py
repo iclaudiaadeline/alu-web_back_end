3e8cadaa9ca01e7f1d6c3655a7b879478b9d3ecd
iclaudiaadeline <a.iradukund3@alustudent.com>
2026-06-26 11:51:28 +0200

    updated

--- i18n/5-app.py
+++ i18n/5-app.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
-"""Task 5"""
-from flask import Flask, g, render_template, request
-from flask_babel import Babel
+"""
+A Flask web application that mocks a user login system and displays.
+"""
+
+from flask import Flask, request, g, render_template
+from typing import Optional, Dict, Any
 
 app = Flask(__name__)
-babel = Babel(app)
-users = {
+
+users: Dict[int, Dict[str, Optional[str]]] = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
     3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
@@ -13,45 +16,37 @@ users = {
 }
 
 
-class Config(object):
-    """simple configuration"""
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
-app.config.from_object(Config)
-
-
-def get_user():
-    """get user from header"""
-    id = request.args.get('login_as')
+def get_user() -> Optional[Dict[str, Any]]:
+    """
+    Get user by ID from the users dictionary.
+    :return: A user dictionary if found, otherwise None.
+    """
     try:
-        return users.get(int(id))
-    except Exception:
+        user_id: Optional[str] = request.args.get('login_as')
+        if user_id is not None:
+            return users.get(int(user_id))
+    except (ValueError, TypeError):
         return None
+    return None
 
 
 @app.before_request
-def before_request():
-    """Before request used to stash user"""
+def before_request() -> None:
+    """
+    Set user information globally before each request.
+    """
     g.user = get_user()
 
 
-@babel.localeselector
-def get_locale():
-    """locale selector determining lang use for template"""
-    loc= request.args.get('locale')
-    if loc and loc in app.config['LANGUAGES']:
-        return loc
-    return request.accept_languages.best_match(Config.LANGUAGES)
-
-
-@app.route("/", methods=['GET'])
-def index():
-    """index rotue"""
-    return render_template("6-index.html")
+@app.route('/')
+def index() -> str:
+    """
+    Render the index page.
+    :return: Rendered HTML content.
+    """
+    return render_template('5-index.html')
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(debug=True)
+    
